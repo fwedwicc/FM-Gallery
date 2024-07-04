05587f0afd44f1f9468527be26b8e6f783ce3190
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-07-04 21:13:43 +0800

    Just added gsap scale animation in some components and also a bit modification for hero layout

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -1,7 +1,81 @@
 gsap.registerPlugin(ScrollTrigger);
 
 const createScrollAnimation = (start, end) => {
+  // ---------  Navigation bar ---------
+  gsap.to('.nav-left',
+    { // to
+      opacity: 1,
+      scrollTrigger: {
+        trigger: '.nav-left',
+        toggleActions: 'restart pause reverse pause',
+        scrub: 1,
+        markers: false,
+        start: 'top 5%',
+        end: 'bottom -10%',
+      },
+      ease: 'none',
+      x: -100,
+      duration: 3,
+    }
+  );
+  gsap.to('.nav-right',
+    { // to
+      opacity: 1,
+      scrollTrigger: {
+        trigger: '.nav-right',
+        toggleActions: 'restart pause reverse pause',
+        scrub: 1,
+        markers: false,
+        start: 'top 5%',
+        end: 'bottom -10%',
+      },
+      ease: 'none',
+      x: 100,
+      duration: 3,
+    }
+  );
+  // ---------  Heading ---------
+  gsap.fromTo('.heading',
+    {
+      opacity: 1,
+      scale: 1 // Start with default size
+    }, // from
+    { // to
+      opacity: 0,
+      scale: 0.8, // End with smaller size
+      scrollTrigger: {
+        trigger: '.heading',
+        toggleActions: 'restart pause reverse pause',
+        scrub: 1,
+        markers: false,
+        start: 'top 30%',
+        end: 'bottom 10%',
+      },
+      ease: 'none',
+      duration: 3,
+    }
+  );
   // ---------  Section 1 ---------
+  gsap.fromTo('.img-1',
+    {
+      opacity: 0,
+      scale: 0.9 // Start with default size
+    }, // from
+    { // to
+      opacity: 1,
+      scale: 1, // End with smaller size
+      scrollTrigger: {
+        trigger: '.img-1',
+        toggleActions: 'restart pause reverse pause',
+        scrub: 1,
+        markers: false,
+        start: 'top 50%',
+        end: 'bottom 90%',
+      },
+      ease: 'none',
+      duration: 3,
+    }
+  );
   gsap.fromTo('.desc-1',
     { opacity: 0 }, // from
     { // to
@@ -19,6 +93,26 @@ const createScrollAnimation = (start, end) => {
     }
   );
   // --------- Section 2 ---------
+  gsap.fromTo('.img-2',
+    {
+      opacity: 0,
+      scale: 0.9 // Start with default size
+    }, // from
+    { // to
+      opacity: 1,
+      scale: 1, // End with smaller size
+      scrollTrigger: {
+        trigger: '.img-2',
+        toggleActions: 'restart pause reverse pause',
+        scrub: 1,
+        markers: false,
+        start: 'top 50%',
+        end: 'bottom 90%',
+      },
+      ease: 'none',
+      duration: 3,
+    }
+  );
   gsap.fromTo('.desc-2',
     { opacity: 0 }, // from
     { // to
