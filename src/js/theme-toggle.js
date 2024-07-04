a925b9bdeac9bf07b2eeb50ac1987b1df8172e2b
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-07-04 17:41:31 +0800

    Just added setup for theme toggle mode

---
+++ src/js/theme-toggle.js
@@ -0,0 +1,37 @@
+const themeToggleBtn = document.getElementById('theme-toggle');
+const darkIcon = document.getElementById('theme-toggle-dark-icon');
+const lightIcon = document.getElementById('theme-toggle-light-icon');
+
+// Change the icons inside the button based on previous settings
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.documentElement.classList.add('dark');
+  darkIcon.classList.remove('hidden');
+} else {
+  document.documentElement.classList.remove('dark');
+  lightIcon.classList.remove('hidden');
+}
+
+themeToggleBtn.addEventListener('click', function () {
+  // toggle icons inside the button
+  darkIcon.classList.toggle('hidden');
+  lightIcon.classList.toggle('hidden');
+
+  // if set via local storage previously
+  if (localStorage.getItem('color-theme')) {
+    if (localStorage.getItem('color-theme') === 'light') {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('color-theme', 'dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('color-theme', 'light');
+    }
+  } else {
+    if (document.documentElement.classList.contains('dark')) {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('color-theme', 'light');
+    } else {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('color-theme', 'dark');
+    }
+  }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // or 'media' if you prefer automatic dark mode based on user's OS setting
   content: ["./src*.{html,js}", "./*.{html,js}"],
 
   theme: {
