801584f00d829deb676d251d34ef9dcaff66a097
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-07-11 15:37:52 +0800

    Fixed the dates, also added inital sequence in comment

--- src/data/constant.js
+++ src/data/constant.js
@@ -3,7 +3,7 @@ const sections = [
   {
     title: "Dalí",
     quote: '"Surrealism is destructive, but it destroys only what it considers to be shackles limiting our vision."',
-    date: "2022",
+    date: "09-15-20",
     imageUrl: "/src/assets/salvador-dali-fmPortrait.jpg",
     imageId: "img-1",
     descId: "desc-1",
@@ -12,7 +12,7 @@ const sections = [
   {
     title: "Einstein",
     quote: '"Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, stimulating progress, giving birth to evolution."',
-    date: "2022",
+    date: "07-10-20",
     imageUrl: "/src/assets/albert-einstein-fmPortrait.jpg",
     imageId: "img-2",
     descId: "desc-2",
@@ -21,16 +21,25 @@ const sections = [
   {
     title: "Joker",
     quote: '"I used to think that my life was a tragedy, but now I realize, it’s a comedy."',
-    date: "2022",
+    date: "06-05-20",
     imageUrl: "/src/assets/joker-fmPortfolio.jpg",
     imageId: "img-3",
     descId: "desc-3",
     altText: "Joker FM Portrait"
   },
   {
+    title: "Mercury",
+    quote: '"The only advice anybody can give is if you wanna be a writer, keep writing. And read all you can, read everything."',
+    date: "05-25-20",
+    imageUrl: "/src/assets/stan-lee-fmPortfolio.jpg",
+    imageId: "img-4",
+    descId: "desc-4",
+    altText: "Stan Lee FM Portrait"
+  },
+  {
     title: "Stan",
     quote: '"The only advice anybody can give is if you wanna be a writer, keep writing. And read all you can, read everything."',
-    date: "2022",
+    date: "05-10-20",
     imageUrl: "/src/assets/stan-lee-fmPortfolio.jpg",
     imageId: "img-4",
     descId: "desc-4",
@@ -39,12 +48,17 @@ const sections = [
   {
     title: "Logan",
     quote: '"Nature made me a freak. Man made me a weapon. And God made it last too long."',
-    date: "2022",
+    date: "04-30-20",
     imageUrl: "/src/assets/logan-fmPortfolio.jpg",
     imageId: "img-5",
     descId: "desc-5",
     altText: "Logan FM Portrait"
   }
+  // Jackie / 04-25-20
+  // RDJ / 04-18-20
+  // Freeman / 04-14-20
+  // Shwesceneger / 04-9-20
+  // Ledger / 04-01-20
 ];
 
 // Function to create a section HTML
