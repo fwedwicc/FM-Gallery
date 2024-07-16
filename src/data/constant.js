ed9a3689de161d03037bb1339a7fa2d00623e479
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-07-16 09:57:33 +0800

    Updated again the image paths for deploying

--- src/data/constant.js
+++ src/data/constant.js
@@ -4,7 +4,7 @@ const sections = [
     title: "Dalí",
     quote: '"Surrealism is destructive, but it destroys only what it considers to be shackles limiting our vision."',
     date: "09-15-20",
-    imageUrl: "/assets/salvador-dali-fmPortrait.jpg",
+    imageUrl: "./src/assets/salvador-dali-fmPortrait.jpg",
     imageId: "img-1",
     descId: "desc-1",
     altText: "Salvadro Dalí FM Portrait"
@@ -13,7 +13,7 @@ const sections = [
     title: "Einstein",
     quote: '"Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, stimulating progress, giving birth to evolution."',
     date: "07-10-20",
-    imageUrl: "/assets/albert-einstein-fmPortrait.jpg",
+    imageUrl: "./src/assets/albert-einstein-fmPortrait.jpg",
     imageId: "img-2",
     descId: "desc-2",
     altText: "Albert Einstein FM Portrait"
@@ -22,7 +22,7 @@ const sections = [
     title: "Joker",
     quote: '"I used to think that my life was a tragedy, but now I realize, it’s a comedy."',
     date: "06-05-20",
-    imageUrl: "/assets/joker-fmPortrait.jpg",
+    imageUrl: "./src/assets/joker-fmPortrait.jpg",
     imageId: "img-3",
     descId: "desc-3",
     altText: "Joker FM Portrait"
@@ -31,7 +31,7 @@ const sections = [
     title: "Mercury",
     quote: '"The most important thing is to live a fabulous life. As long as it’s fabulous, I don’t care how long it is."',
     date: "05-25-20",
-    imageUrl: "/assets/mercury-fmPortrait.jpg",
+    imageUrl: "./src/assets/mercury-fmPortrait.jpg",
     imageId: "img-4",
     descId: "desc-4",
     altText: "Mercury FM Portrait"
@@ -40,7 +40,7 @@ const sections = [
     title: "Stan",
     quote: '"The only advice anybody can give is if you wanna be a writer, keep writing. And read all you can, read everything."',
     date: "05-10-20",
-    imageUrl: "/assets/stan-lee-fmPortrait.jpg",
+    imageUrl: "./src/assets/stan-lee-fmPortrait.jpg",
     imageId: "img-5",
     descId: "desc-5",
     altText: "Stan Lee FM Portrait"
@@ -49,7 +49,7 @@ const sections = [
     title: "Logan",
     quote: '"Nature made me a freak. Man made me a weapon. And God made it last too long."',
     date: "04-30-20",
-    imageUrl: "/assets/logan-fmPortrait.jpg",
+    imageUrl: "./src/assets/logan-fmPortrait.jpg",
     imageId: "img-6",
     descId: "desc-6",
     altText: "Logan FM Portrait"
@@ -58,7 +58,7 @@ const sections = [
     title: "Jackie",
     quote: '"I’m not afraid of dying. I’m afraid of not trying."',
     date: "04-25-20",
-    imageUrl: "/assets/jackie-fmPortrait.jpg",
+    imageUrl: "./src/assets/jackie-fmPortrait.jpg",
     imageId: "img-7",
     descId: "desc-7",
     altText: "Jackie FM Portrait"
@@ -67,7 +67,7 @@ const sections = [
     title: "RDJ",
     quote: '"Sometimes you gotta run before you can walk."',
     date: "04-18-20",
-    imageUrl: "/assets/rdj-fmPortrait.jpg",
+    imageUrl: "./src/assets/rdj-fmPortrait.jpg",
     imageId: "img-8",
     descId: "desc-8",
     altText: "RDJ FM Portrait"
@@ -76,7 +76,7 @@ const sections = [
     title: "Freeman",
     quote: '"I can say that life is good to me. Has been and is good. So I think my task is to be good to it. So how do you be good to life? You live it."',
     date: "04-14-20",
-    imageUrl: "/assets/freeman-fmPortrait.jpg",
+    imageUrl: "./src/assets/freeman-fmPortrait.jpg",
     imageId: "img-9",
     descId: "desc-9",
     altText: "Freeman FM Portrait"
@@ -85,7 +85,7 @@ const sections = [
     title: "Schwarzenegger",
     quote: '"The mind is the limit. As long as the mind can envision the fact that you can do something, you can do it, as long as you really believe 100 percent."',
     date: "04-09-20",
-    imageUrl: "/assets/schwarzenegger-fmPortrait.jpg",
+    imageUrl: "./src/assets/schwarzenegger-fmPortrait.jpg",
     imageId: "img-10",
     descId: "desc-10",
     altText: "Schwarzenegger FM Portrait"
@@ -94,7 +94,7 @@ const sections = [
     title: "Ledger",
     quote: '"You see, in their last moments, people show you who they really are."',
     date: "04-01-20",
-    imageUrl: "/assets/ledger-fmPortrait.jpg",
+    imageUrl: "./src/assets/ledger-fmPortrait.jpg",
     imageId: "img-11",
     descId: "desc-11",
     altText: "Ledger FM Portrait"
@@ -103,7 +103,7 @@ const sections = [
     title: "David",
     quote: '"Every block of stone has a statue inside it and it is the task of the sculptor to discover it."',
     date: "03-21-20",
-    imageUrl: "/assets/michaelangelo-fmPortrait.jpg",
+    imageUrl: "./src/assets/michaelangelo-fmPortrait.jpg",
     imageId: "img-12",
     descId: "desc-12",
     altText: "David FM Portrait"
